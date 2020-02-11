1f8d6a497f61408bf5b9972a3a0c6dcb7a2084b6
shivKjaju <shivkumarsagshi@gmail.com>
2020-02-10 21:32:23 -0600

    project summary nested in projectlist

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,47 +1,14 @@
 import React from 'react'
 
+import ProjectSummary from './ProjectSummary'
+
 const ProjectList = () =>{
     return (
         <div className="list section">
-            <div className="card z-depth-0 summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by SPJ</p>
-                    <p className="grey-text"> 9th Febraury</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by SPJ</p>
-                    <p className="grey-text"> 9th Febraury</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by SPJ</p>
-                    <p className="grey-text"> 9th Febraury</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by SPJ</p>
-                    <p className="grey-text"> 9th Febraury</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by SPJ</p>
-                    <p className="grey-text"> 9th Febraury</p>
-                </div>
-            </div>
+           <ProjectSummary/>
+           <ProjectSummary/>
+           <ProjectSummary/>
+           <ProjectSummary/>
         </div>
     )
 }
