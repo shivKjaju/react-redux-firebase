a6e4bb93022206b60b6fb341aac84736156d28f4
shivKjaju <shivkumarsagshi@gmail.com>
2020-02-12 21:37:40 -0600

    styling added

--- src/components/layout/SignedIn.js
+++ src/components/layout/SignedIn.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const SignedIn = () =>{
     return (
        <ul className="right">
-           <li><NavLink to='/'>New Project</NavLink></li>
+           <li><NavLink to='/create'>New Project</NavLink></li>
            <li><NavLink to='/'>Log Out</NavLink></li>
            <li><NavLink to='/' className="btn btn-floating Pink darken-3">SPJ</NavLink></li>
        </ul>
