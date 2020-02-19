e05e67aaa6f2c074d151c3a4b18e824151a36497
shivKjaju <shivkumarsagshi@gmail.com>
2020-02-18 21:23:18 -0600

    redux store now synced in with firestore and dashboard fetching data from the firestore via redux

--- src/components/dashboard/DashBoard.js
+++ src/components/dashboard/DashBoard.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
 import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 class DashBoard extends Component{
     render(){
@@ -26,8 +28,12 @@ class DashBoard extends Component{
 
 
 const mapStateToProps = (state) =>{
+    console.log("The state is ", state)
     return{
-        projects: state.project.projects
+        projects: state.firestore.ordered.projects
     }
 }
-export default connect(mapStateToProps) (DashBoard)
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([{collection: 'projects'}])
+) (DashBoard)
