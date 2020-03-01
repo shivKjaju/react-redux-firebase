df2f3acb4cf8efa4aaadb67915b7f997c7b2c332
shivKjaju <shivkumarsagshi@gmail.com>
2020-03-01 09:03:05 -0600

    change navbar according to auth status

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,22 +7,22 @@ import SignedOut from './SignedOut'
 import { connect } from 'react-redux'
 
 
-const Navbar = () =>{
+const Navbar = (props) =>{
+    const { auth } = props
+    const links = auth.uid ? <SignedIn/> : <SignedOut/>
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">ProjectPlan</Link>
-                <SignedIn/>
-                <SignedOut/>
+                { links }
             </div>
         </nav> 
     )
 }
 
 const mapStateToProps= (state) =>{
-    console.log(state);
     return{
-
+        auth: state.firebase.auth
     }
 }
 export default connect (mapStateToProps)(Navbar)
