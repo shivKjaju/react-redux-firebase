fcccea1023478ff771f945b03a62d0b3ae0f8110
Shiv <shivkumarsagshi@gmail.com>
2020-03-15 15:07:39 -0500

    deployed to firebase and removed all console logs

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -5,32 +5,27 @@ const initState = {
 const authReducer = (state= initState, action) => {
     switch(action.type){
         case 'LOGIN_ERROR':
-            console.log("Login error")
             return {
                 ...state,
                 authError: 'Login failed'
             }
 
         case 'LOGIN_SUCCESS':
-            console.log("Login success")
             return {
                 ...state,
                 authError: null
             }
 
         case 'SIGNOUT_SUCCESS':
-            console.log("Sign out success")
             return state
            
         case 'SIGNUP_SUCCESS':
-            console.log("Sign up success")
             return {
                 ...state,
                 authError: null
             }
 
         case 'SIGNUP_ERROR':
-            console.log("Sign up error")
             return {
                 ...state,
                 authError: action.err.message
